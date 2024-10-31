e44c1d3b080a4cae1227ce15927956d76c42be21
gehad-abdulaziz <Jihad_20230160@fci.helwan.edu.eg>
2024-10-31 17:46:11 +0300

    Update main.c

--- DoublyList/main.c
+++ DoublyList/main.c
@@ -1,3 +1,9 @@
+/*
+
+وما توفيقي إلا بالله العلي العظيم
+
+@Gehad Abdulaziz
+*/
 #include <stdio.h>
 #include "doubly.h"
 void print(LinkedType e)
@@ -9,13 +15,22 @@ int main()
     Linkedlist lq;
     CreateList(&lq);
     int q=0;
-    for(int i =10 ;i>=0; i--)
+    for(int i =0 ;i<10; i++)
     Insert(&lq , i, q++);
-    Insert(&lq , 78, q++);
-    Insert(&lq , 65, q++);
-    Insert(&lq ,47, q++);
-    Insert(&lq , 24, q++);
+    int y;
+    TraverseList(&lq , &print);printf("\n ---DELETE FIRST ELEMENT---\n");
 
+    Retrieve(&lq , &y , 0);
+    printf("\n---%d---\n" , y);
+    printf("\nprint queue with function i made in implementation level : \n");
+    TraverseList(&lq , &print);
+    printf("\nprint revesre queue with function i made in implementation level : \n");
+    ReverseTraverseList(&lq, &print);
+    printf("\n---------------------------------------\n");
+    TraverseList(&lq , &print);
+    printf("\n ---DELETE LAST ELEMENT---\n");
+    Retrieve(&lq , &y , ListSize(&lq)-1);
+    printf("\n---%d---\n" , y);
     printf("\nprint queue with function i made in implementation level : \n");
     TraverseList(&lq , &print);
     printf("\nprint revesre queue with function i made in implementation level : \n");
